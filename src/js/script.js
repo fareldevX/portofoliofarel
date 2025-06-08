ddacf6dddff2114f89709a6d0d774df650e4f321
fareldevX <farelmaster01@gmail.com>
2025-06-08 12:37:24 +0700

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
+// bars click
+const bars = document.getElementById("bars");
+const text = document.querySelector(".text");
 
+bars.addEventListener("click", () => {
+  text.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!text.contains(e.target) && !bars.contains(e.target)) {
+    text.classList.remove("active");
+  }
+});
